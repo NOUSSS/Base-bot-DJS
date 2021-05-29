3278304a990393a87e0db295948cc7b57cd35373
Nouss <noussmail@protonmail.com>
2021-05-29 21:55:44 +0200

    Console log + setActivity

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,10 @@
 module.exports = {
     name: 'ready',
     run : async (client) => {
+
         console.clear();
         console.log(`[CLIENT]`.red + ` Connecté sur > ` + `${client.user.tag} (${client.user.id})`.red);
         client.user.setActivity('PRESENCE');
-    }
-}
+        
+    },
+};
